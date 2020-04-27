222b36c97212bafc4727d1532e8f8bab29b78b17
Albert Li <Albert.Li@isapsolution.com>
2020-04-27 16:59:45 +0800

    output declaration file for handlers

---
+++ src/handlers/index.ts
@@ -0,0 +1,9 @@
+export { RabbitMQChannelCancelledError } from './channels/rabbitmq-channel.cancel';
+export { RabbitMQChannelClosedError } from './channels/rabbitmq-channel.close';
+export { RabbitMQChannelError } from './channels/rabbitmq-channel.error';
+export { RabbitMQConnectionClosedError } from './connections/rabbitmq-connection.close';
+export { RabbitMQConnectionError } from './connections/rabbitmq-connection.error';
+export { RabbitMQConsumerSetupError } from './consumers/rabbitmq-consumer-setup.error';
+export { RabbitMQPublisherConfirmationError } from './publishers/rabbitmq-publisher-confirmation.error';
+export { RabbitMQPublisherSetupError } from './publishers/rabbitmq-publisher-setup.error';
+export { RabbitMQReconnectError } from './reconnects/rabbitmq-reconnect.error';
